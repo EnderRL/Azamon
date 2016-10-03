a312c539dbf043561f35236876a011276294f339
carlos.roldan <carlosroldanmontaner@gmail.com>
2016-10-03 17:06:45 +0200

    Hola

--- src/IA/Estado.java
+++ src/IA/Estado.java
@@ -2,17 +2,31 @@ package IA;
 
 import IA.Azamon.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Estado {
     private int felicidad, precio;
 
     private ArrayList<PaqueSet> paquetesOfertados;
     public static Transporte ofertas;
+    public static Paquetes paquetes;
+
 
     public Estado(ArrayList<PaqueSet> paquetesOfertados, int felicidad, int precio) {
         this.paquetesOfertados = paquetesOfertados;
         this.felicidad = felicidad;
         this.precio = precio;
+    }
+
+    private void metePaquete(int paquete, Random random) {
+        int transporteRandom = random.nextInt();
+        if (paquetesOfertados.get(transporteRandom).getPeso() + Paquetes. ) {
+
+        }
+    }
+
+    public Estado(int seed) {
+        Random random = new Random(seed);
     }
 
     public int getFelicidad() {
