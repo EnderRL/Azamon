26c01a8a3122940383903d1adb6980235a55993b
Juan Miguel <juanmiguel1996@gmail.com>
2016-10-05 21:24:27 +0200

    Hola

--- src/IA/Main.java
+++ src/IA/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("ESTO ES EL INICIO DE GLA2, que dominará el mundo");
         System.out.println("Creando paquetes. :D");
-        Paquetes paquetes = new Paquetes(400, (int)Parametros.seed);
+        Paquetes paquetes = new Paquetes(1000, (int)Parametros.seed);
         System.out.println("Creando transportes. :D:D");
-        Transporte ofertas = new Transporte(paquetes, 1, (int)Parametros.seed);
+        Transporte ofertas = new Transporte(paquetes, 1.2, (int)Parametros.seed);
         Estado.setOfertas(ofertas);
         Estado.setPaquetes(paquetes);
         Estado estado = new Estado((int)Parametros.seed);
