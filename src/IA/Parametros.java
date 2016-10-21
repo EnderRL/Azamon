25b0f88e1421abeb92aa976baed4ca038f5b5b84
Daryan7 <juanmiguel1996@gmail.com>
2016-10-21 10:11:41 +0200

    Funciona yeah

--- src/IA/Parametros.java
+++ src/IA/Parametros.java
@@ -1,6 +1,6 @@
 package IA;
 
 public class Parametros {
-    final static int seed = (int) System.nanoTime();
-    //final static int seed = 1234;
+    //final static int seed = (int) System.nanoTime();
+    final static int seed = 1234;
 }
