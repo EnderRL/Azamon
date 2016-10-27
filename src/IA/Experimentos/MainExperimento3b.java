99bfbd8f1a5658b26bdbeb50d276448622aa6a98
mario.fernandez <mario.fernandez@est.fib.upc.edu>
2016-10-27 17:01:34 +0200

    Funciona yeah

--- src/IA/Experimentos/MainExperimento3b.java
+++ src/IA/Experimentos/MainExperimento3b.java
@@ -3,23 +3,19 @@ package IA.Experimentos;
 import IA.Azamon.Paquetes;
 import IA.Azamon.Transporte;
 import IA.Estado;
-import IA.Generadores.GeneradorSucesoresHillClimbing;
-import IA.Generadores.GeneradorSucesoresMover;
 import IA.Generadores.GeneradorSucesoresMoverSA;
 import IA.Heuristicos.FuncionHeuristicaPrecio;
 import IA.Utils.Escritor;
-import IA.Utils.Parametros;
 import aima.search.framework.Problem;
 import aima.search.framework.SearchAgent;
-import aima.search.informed.HillClimbingSearch;
 import aima.search.informed.SimulatedAnnealingSearch;
 
 /**
  * Created by mario.fernandez on 27/10/2016.
  */
-public class MainExperimento3 {
+public class MainExperimento3b {
     public static void main(String[] argv) {
-        Escritor escritor = new Escritor("resultadosExperimento3.txt");
+        Escritor escritor = new Escritor("resultadosExperimento3b.txt");
         escritor.write("K LAMBDA PRECIO PASOS TIEMPO\n");
         for (int i = 0; i < 25; ++i) {
             Paquetes paquetes = new Paquetes(100, (int)System.nanoTime());
@@ -34,15 +30,15 @@ public class MainExperimento3 {
 
             Problem problem = new Problem(estadoInicial, new GeneradorSucesoresMoverSA((int)System.nanoTime()), state -> true, new FuncionHeuristicaPrecio());
             try {
-                //K FIJA EN 10
-                k = 10;
-                //VARIAMOS LAMBDA
-                for (lambda = 0.0001; lambda <= 0.1; lambda *= 10) {
+                //LAMBDA FIJA EN 0.0001
+                lambda = 0.001;
+                //VARIAMOS K
+                for (k = 5; k <= 50000; k *= 10) {
                     //REPETIMOS PARA HACER MEDIAS
                     double mediaPrecio = 0;
                     int mediaPasos = 0;
                     long mediaTiempo = 0;
-                    SimulatedAnnealingSearch simulatedAnnealingSearch = new SimulatedAnnealingSearch(4000,100,k,lambda);
+                    SimulatedAnnealingSearch simulatedAnnealingSearch = new SimulatedAnnealingSearch(1400,100,k,lambda);
                     for (int j = 0; j < 5; ++j) {
                         long time = System.nanoTime();
                         SearchAgent searchAgent = new SearchAgent(problem, simulatedAnnealingSearch);
